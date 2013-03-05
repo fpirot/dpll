a6d5949bae7f2e745509d74f1e99d52b440955a9
François Pirot <francois.pirot@ens-lyon.fr>
2013-03-05 14:36:33 +0100

    dpll avec les commentaires et qui compile

--- dpll2.ml
+++ dpll2.ml
@@ -1,24 +1,48 @@
-let rec valuation n =
-  let rec aux l = function
-    | 0 -> l
-    | n -> let v = Assig.read n in
-	   if v = 0 then aux (n :: l) (n-1)
-	   else aux (v :: l) (n-1)
-  in aux [] n;;
+exception Unsatisfiable;;
 
-let dpll env = 
-  let rec aux env =
-    if Op.is_empty env then ()
-    else begin
-    let (x, (ltrue, envtrue), (lfalse, envfalse)) = Op.split env in
-    try (
-      Assig.write x x; 
-      let env' = Op.propagation envtrue in
-      aux env')
-    with Unsatisfiable -> (
-      Assig.write x (-x);
-      let env' = Op.propagation envfalse in
-      aux env')
-    end;
-  in aux env; valuation Assig.nbr;;
+module type Assig =
+  sig
+    type t = int
+    val nbr : int
+    val read : int -> int
+    val write : int -> int -> unit
+  end;;
+
+module type Op =
+  sig
+    type env
+    type set
+    val is_empty : env -> bool
+    val split : env -> int * (set list * env) * (set list * env)
+    val propagation : env -> env
+  end;;
+
+module Dpll = functor (Op : Op) -> functor (Assig : Assig) ->
+struct 
+  let rec valuation n =
+    let rec aux l = function
+      | 0 -> l
+      | n -> let v = Assig.read n in
+	     if v = 0 then aux (n :: l) (n-1)
+	     else aux (v :: l) (n-1)
+    in aux [] n
+(* Une fois que l'on a fini d'assigner les variables et que l'instance d'entrée est satisfaite, renvoie une liste d'assignations qui satisfait le problème, à partir des données entrées dans Assig. n est le nombre de variables du problème. *)
+
+  let dpll env = 
+    let rec aux env =
+      if Op.is_empty env then ()
+      else begin
+	let (x, (ltrue, envtrue), (lfalse, envfalse)) = Op.split env in
+	try (
+	  Assig.write x x; 
+	  let env' = Op.propagation envtrue in
+	  aux env')
+	with Unsatisfiable -> (
+	  Assig.write x (-x);
+	  let env' = Op.propagation envfalse in
+	  aux env')
+      end;
+    in aux env; valuation Assig.nbr
+(* Renvoie une assignation qui permet de satisfaire l'instance d'entrée, ou l'exception Unsatisfiable si cette dernière n'est pas satisfiable. *)
   
+end;;
