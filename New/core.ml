4d440305aad5b0334ffcf2f37c0c11eb5683e661
Benjamin Farinier <benjamin.farinier@ens-lyon.fr>
2013-03-07 22:17:04 +0100

    Remodif core

--- New/core.ml
+++ New/core.ml
@@ -23,33 +23,33 @@ struct
   let add x l = if List.exists (fun y -> y = -x) l then raise Satisfiable else x::l
 
 
-  let init string n channel =
+  let init str n channel =
     let rec next () = match Scanf.bscanf channel " %d " (fun x -> x) with
       | 0 -> ()
       | n -> next ()
 	
-    and iter s list lstC ensV = function
+    and iter s c lst lstC ensV = function
       | 0 -> let s = try Scanf.bscanf channel "c %s@\n" (fun x -> s ^ x ^ "\n") with _ -> s in
 	     Scanf.bscanf channel " %d "
-	       (fun x -> if x = 0 then (ensV :: lstC, list, s)
+	       (fun x -> if x = 0 then (c + 1, ensV :: lstC, lst, s)
 		 else begin
-		   try iter s (insert (abs x) list) lstC (add x ensV) 0 with
-		       Satisfiable -> (lstC, list, s) end)
+		   try iter s c (insert (abs x) lst) lstC (add x ensV) 0 with
+		       Satisfiable -> (c, lstC, lst, s) end)
       | n -> let s = try Scanf.bscanf channel "c %s@\n" (fun x -> s ^ x ^ "\n") with _ -> s in
 	     Scanf.bscanf channel " %d "
 	       (fun x -> if x = 0 then
-		   iter s list (ensV :: lstC) [] (n - 1)
+		   iter s c lst (ensV :: lstC) [] (n - 1)
 		 else begin
-		   try iter s (insert (abs x) list) lstC (add x ensV) n with
+		   try iter s c (insert (abs x) lst) lstC (add x ensV) n with
 		       Satisfiable ->
-			 (next (); iter s list lstC [] (n - 1)) end) in
+			 (next (); iter s (c - 1) lst lstC [] (n - 1)) end) in
     
-    iter string [] [] [] n;;
+    iter str n [] [] [] n;;
 
 
   let load channel =
     let s = try Scanf.bscanf channel "c %s@\n" (fun x -> x ^ "\n") with _ -> "" in
-    Scanf.bscanf channel "p cnf %d %d" (fun v c -> (v, c, init s (c-1) channel))
+    Scanf.bscanf channel "p cnf %d %d" (fun v c -> (v, init s (c-1) channel))
 
 end;;
 
@@ -62,7 +62,7 @@ module Core =
     type order = (int * int) list
     
     let debug = true
-    let (var, cls, (lst, ord, comment)) = Load.load (Scanf.Scanning.open_in ("Test/"^(try Sys.argv.(1) with _ -> "test")))
+    let (var, (cls, lst, ord, comment)) = Load.load (Scanf.Scanning.open_in ("Test/"^(try Sys.argv.(1) with _ -> "test")))
     let wlit = try (Sys.argv.(2) = "wlit") with _ -> false
     
     
