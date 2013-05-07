d3425e60c3b766d604000f80633ecf4eff1c8a45
Benjamin Farinier <benjamin.farinier@ens-lyon.fr>
2013-05-07 14:09:15 +0200

    lecture.ml

---
+++ lecture.ml
@@ -0,0 +1,27 @@
+(* Module de, surprise, chargement des donnees *)
+
+module Load =
+struct
+
+  exception Useless
+
+	let edge channel graph = Scanf.bscanf channel " %d %d"
+    (fun u v -> graph.(u) <- v::(graph.(u));	graph.(v) <- u::(graph.(v)))
+
+	let rec read channel graph = function
+		|0 -> graph
+		|n ->	Scanf.bscanf channel "%c"
+				(fun c -> match c with
+					|'e' -> edge channel graph; read channel graph (n-1)
+					|_ -> Scanf.bscanf channel "%[^\n]\n" (fun _ -> ()); read channel graph n)
+
+	let init channel n e =
+		let graph = Array.make n [] in
+			read channel graph e
+			
+  let load channel =
+		Scanf.bscanf channel "p edge %d %d" (fun n e -> n, init channel n e)
+
+end;;
+
+Load.load (Scanf.Scanning.open_in "test");;
