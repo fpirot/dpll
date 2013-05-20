cd4a30f083443f7bd95be11b0344e1ec9ce97365
François Pirot <francois.pirot@ens-lyon.fr>
2013-05-20 12:58:14 +0200

    correction de dlis (je sais pas exactement ce qui se passe, mais j'ai tout rééquilibré)

--- dlis.ml
+++ dlis.ml
@@ -18,6 +18,9 @@ struct
 
   let debug = false
 
+  let print_order ord = print_string "Order:\n";
+    Map.iter (fun x v -> print_int x; print_string ": "; print_float v; print_newline()) ord
+
   type order = float Map.t
   (* Table d'association, qui a x associe le score obtenu par ce litéral. *)
 
@@ -31,34 +34,37 @@ struct
   let is_empty = Map.is_empty
 
   let remove = List.fold_right 
-    (fun c mp -> let n = Cor.length c in let v = power2 (-n) in
-		 Cor.cls_fold (fun x m -> let v1 = (try Map.find x m with Not_found -> 0.) -. v in
-					  if v1 > 0. then Map.add x v1 m else Map.remove x m) c mp)
-  (* Retire une liste de clauses à prendre en considération dans la map. *)
+    (fun c mp -> let n = (Cor.length c) + 1 in let v = power2 (-n-1) in
+		 Cor.cls_fold (fun x m -> let v1 = (try (Map.find x m) -. v  with Not_found -> 0.) -. v in
+					  (*if v1 > 0. then*) Map.add x v1 m (*else Map.remove x m*)) c mp)
+  (* Retire une liste de clauses à prendre en considération dans la map
+     (il faut se rappeler que ces clauses viennent de se voir attribuer 
+     un literal à vrai, et donc que leur taille a diminué d'un cran) *)
 
   let decr_size = List.fold_right 
-    (fun c mp -> let n = Cor.length c in
-		 Cor.cls_fold (fun x m -> let v = try Map.find x m with Not_found -> 0. in
-					  let v1 = if v > 0. then v +. power2 (-n) else power2 (1-n) in
-					  Map.add x v1 m) c mp)
-  (* Prend en compte une liste de clauses dont la taille va être décrémentée. *)
+    (fun c mp -> let n = Cor.length c in let v = power2 (-n-1) in
+		 Cor.cls_fold (fun x m -> let v1 = try (Map.find x m) +. v with Not_found -> 0. in
+					  (*if v1 > 0. then*) Map.add x v1 m (*else m*)) c mp)
+  (* Prend en compte une liste de clauses dont la taille vient d'être décrémentée. *)
 
   let add = List.fold_right
     (fun c mp -> let n = Cor.length c in let v = power2 (-n) in
 		 Cor.cls_fold (fun x m -> Map.add x ((try Map.find x m with Not_found -> 0.) +. v) m) c mp)
   (* Ajoute une liste de clauses à prendre en considération dans la map. *)
 
-  let extract ord = let xdlis = fst (Map.fold (fun x v (xm, max) -> if (Cor.read x = 0 && v > max) then (x,v) else (xm, max)) ord (0,0.)) in
-  if xdlis = 0 then raise Not_found else xdlis
-(* Renvoie le choix xdlis selon l'ordre. *)
+  let extract ord = if debug then print_order ord;
+    let xdlis = fst (Map.fold (fun x v (xm, max) -> if (Cor.read x = 0 && v > max) then (x,v) else (xm, max)) ord (0,0.)) in
+    if xdlis = 0 then raise Not_found else xdlis
+  (* Renvoie le choix xdlis selon l'ordre. *)
 
   let update x (l1, l2) ord =
-    decr_size l2 (remove l1 (Map.remove (-x) (Map.remove x ord)))
+    let ord = decr_size l2 (remove l1 (Map.remove (-x) (Map.remove x ord))) in
+    if debug then print_order ord; ord
   (* Met l'ordre à jour lorsque l'on affecte le litéral x à vrai. *)
 
   let create () =
     let rec make_list l = function
-      |0 -> Cor.cls_make 0 :: l
+      |0 -> l
       |n -> make_list (Cor.cls_make (n-1) :: l) (n-1) in
     let l = make_list [] (Cor.nb_cls ()) in
     add l Map.empty
