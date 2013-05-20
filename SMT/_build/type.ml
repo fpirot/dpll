d1e8bff08ec546005ba88cb8e2f7dcd85cc87318
François Pirot <francois.pirot@ens-lyon.fr>
2013-05-20 21:34:39 +0200

    nettoyage

--- SMT/_build/type.ml
+++
@@ -1,4 +0,0 @@
-type pterms = PFun of string * pterms | PVar of string | PList of pterms * pterms;;
-type ppredicat = PEqual of pterms * pterms | PDiff of pterms * pterms;;
-type pformule = PPred of ppredicat | POr of pformule * pformule | PAnd of pformule * pformule
-              | PNot of pformule | PImply of pformule * pformule;;
