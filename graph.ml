b384bb85dbe6ba4f352b7c7440ecd693fc2a150a
Benjamin Farinier <benjamin.farinier@ens-lyon.fr>
2013-05-20 16:10:02 +0200

    Correction graph.ml

--- graph.ml
+++ graph.ml
@@ -9,6 +9,7 @@ sig
   val literals : cls -> int list
   val father : int -> cls
   val depth : int -> int
+  val graphe : cls -> (int * int) list
 end;;
 
 module Print = functor (Cor : CoreElt) ->
@@ -22,7 +23,7 @@ struct
 
   let colors channel arr =
     Printf.fprintf channel "\nfalse[style=filled,shape=box,color=crimson];";
-    Array.iteri (fun i x -> if x then (*let v = abs_float (4. *. float (Cor.depth (i+1))) /. (float Cor.var) in*)
+    Array.iteri (fun i x -> if x then
 	Printf.fprintf channel "\n%d[style=filled,color=cornflowerblue];" (Cor.read (i+1))) arr 
   (* Attribue leur couleur aux noeuds du graphe. *)  
     
@@ -37,7 +38,7 @@ struct
 	Printf.fprintf channel "\n%d -> %d;" (Cor.read x) (Cor.read y); currents channel test l
 
 
-  let file cls arr (lcur, loth) =
+  let file cls arr lst (*lcur, loth*) =
     let test =
       let mat = Array.create_matrix Cor.var Cor.var true in
       fun x y -> let b = abs x <> abs y && mat.(abs x - 1).(abs y - 1) in mat.(abs x - 1).(abs y - 1) <- false; b in
@@ -47,10 +48,12 @@ struct
     colors channel arr;
     Printf.fprintf channel "\nsubgraph current_level{";
     Cor.iter (fun x -> Printf.fprintf channel "\n%d -> false" (Cor.read x)) cls;
-    currents channel test lcur;
+    currents channel test lst;
     Printf.fprintf channel "\n}";
+    (*currents channel test lcur;
+    Printf.fprintf channel "\n"};
     others channel test loth;
-    Printf.fprintf channel "\n}";
+    Printf.fprintf channel "\n"};*)
     close_out channel;
 
 end;;
@@ -86,7 +89,7 @@ struct
   type cls = Cor.cls
 
 
-  let draw cls =
+  (*let draw cls =
 
     let arr = Array.make Cor.var false
     and partition a l lst =
@@ -104,15 +107,30 @@ struct
 
     in
     let lst = iterate ([],[]) (Cor.literals cls) in
-    Print.file cls arr lst
+    Print.file cls arr lst*)
+
+  let draw cls lst =
+  
+    let arr = 
       
+      let ar = Array.make Cor.var false
+      and current =
+        let max = ref min_int in
+          Cor.iter (fun x -> if (Cor.depth x) > !max then max := (Cor.depth x)) cls;
+          !max in
+      
+      List.iter (fun (x, y) -> if (Cor.depth x) = current then ar.(abs x - 1) <- true;
+        if (Cor.depth y) = current then ar.(abs y - 1) <- true) lst;
+        
+      ar in
+   Print.file cls arr lst
       
 end;;
 
 module type GraphAbstract = functor (Cor : CoreElt) ->
 sig
 	type cls = Cor.cls
-	val draw : cls  -> unit 
+	val draw : cls -> (int * int) list  -> unit 
 	(*val convert : int list -> cls*)
 end;;
 
